737b20553fd270e6708b16fc7123696d8e2f8adf
xiaolongkuang <xiaolong.kuang@foxmail.com>
2020-07-02 17:19:33 +0800

    入门篇代码

---
+++ Sample01/Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        /// <param name="option"></param>
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View( new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
